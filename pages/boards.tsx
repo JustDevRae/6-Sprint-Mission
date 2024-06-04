fb69a4b4a33fe93757c1f06eed10c3507728746e
JustDevRae <ksrae165@gmail.com>
2024-06-04 09:58:28 +0900

    feat: 반응형에 따른 베스트 게시글 갯수 조절 구현

--- pages/boards.tsx
+++ pages/boards.tsx
@@ -1,4 +1,5 @@
 import axios from "@/lib/axios";
+import { useEffect, useState } from "react";
 import BestArticleSection from "@/components/BestArticleSection";
 import AllArticleSection from "@/components/AllArticleSection";
 
@@ -7,12 +8,51 @@ export async function getServerSideProps() {
   const articles = response.data.list ?? [];
   return {
     props: {
-      articles,
+      initArticles: articles,
     },
   };
 }
 
-export default function CommunityFeedPage({ articles }: any) {
+export default function CommunityFeedPage({ initArticles }: any) {
+  const [pageSize, setPageSize] = useState(3);
+  const [articles, setArticles] = useState(initArticles);
+
+  const getPageSize = () => {
+    const width = window.innerWidth;
+    if (width < 768) {
+      return 1;
+    } else if (width < 1280) {
+      return 2;
+    } else {
+      return 3;
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPageSize(getPageSize());
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await axios.get(
+        `articles/?pageSize=${pageSize}&orderBy=like`
+      );
+      setArticles(response.data.list ?? []);
+    };
+
+    fetchArticles();
+  }, [pageSize]);
+
   return (
     <>
       <BestArticleSection articles={articles} />
