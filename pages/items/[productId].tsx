e7966bb513dfde4e518fc1a97f100dacf1ded862
JustDevRae <ksrae165@gmail.com>
2024-06-07 16:41:47 +0900

    design: 상품 디테일 섹션 레이아웃 구현

--- pages/items/[productId].tsx
+++ pages/items/[productId].tsx
@@ -1,5 +1,7 @@
 import axios from "@/lib/axios";
+import Image from "next/image";
 import stylse from "@/styles/ProductDetailPage.module.css";
+import Heart from "@/assets/images/icons/ic_heart.svg";
 
 export async function getServerSideProps(context: any) {
   const productId = context.params["productId"];
@@ -22,9 +24,51 @@ export default function ProductDetailPage({
   productDetail,
   productComments,
 }: any) {
+  console.log(productDetail);
+  console.log(productComments);
   return (
     <>
-      <div className="detailSection"></div>
+      <div className={stylse.detailSection}>
+        <div className={stylse.imageWrapper}>
+          <Image
+            src={productDetail.images[0]}
+            alt={productDetail.name}
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
+        <div className={stylse.detailWrapper}>
+          <div className={stylse.detailWrapperHeader}>
+            <p className={stylse.productName}>{productDetail.name}</p>
+            <p className={stylse.productPrice}>{productDetail.price}원</p>
+          </div>
+          <div className={stylse.detailWrapperMain}>
+            <div className={stylse.introduceWrapper}>
+              <p className={stylse.label}>상품 소개</p>
+              <p className={stylse.productDescription}>
+                {productDetail.description}
+              </p>
+            </div>
+
+            <div className={stylse.tagWrapper}>
+              <p className={stylse.label}>상품 태그</p>
+              <div className={stylse.tagList}>
+                {productDetail.tags.map((tag: any) => (
+                  <div key={tag} className={stylse.tag}>
+                    {`#${tag}`}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className={stylse.likeCountWrapper}>
+              <Image src={Heart} alt="하트 아이콘" width={32} height={32} />
+              {productDetail.favoriteCount}
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="commentSection"></div>
     </>
   );
